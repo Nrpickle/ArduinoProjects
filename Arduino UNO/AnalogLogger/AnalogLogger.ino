a0d2badab75e94f6572a2a891a3cbcc18b21c630
Nick McComb <nrpickle@gmail.com>
2015-03-06 19:45:16 -0800

    [AL] Added documentation

--- Arduino UNO/AnalogLogger/AnalogLogger.ino
+++ Arduino UNO/AnalogLogger/AnalogLogger.ino
@@ -4,6 +4,8 @@
  Nick McComb [nick@nickmccomb.net]
  March 2015
  
+ @desc 
+ This program records all of the analog ports onto an SD Card. It _requires_ no hardware to run besides the Ethernet Shield, but you can also add a SF RTC on the I2C bus to enable RTC logging.
  */
 
 #include <SPI.h>
@@ -17,8 +19,9 @@ const int trialResetPin = 9;
 const int trialNumAddr = 0;
 const int chipSelect = 4;
 const int maxTrialNum = 250;
+const int delayConstant = 100000;  //Time to wait until data is collected, should change to a timer
 
-String filename;  //Global filename
+String filename;  //Global filename 
 
 short currentTrial;
 
@@ -102,11 +105,11 @@ currentTrial, A0, A1, A2
 */
 void loop()
 {
-  delay(100);
+  delayMicroseconds(delayConstant);
   
   String dataString = "";
   
-  static int iter = 0;
+  static int iter = 0;  //Unused variable?
   
   dataString += String(currentTrial);
   dataString += ",";
@@ -142,87 +145,3 @@ void loop()
     #endif
   }
 }
-
-/*
-
-#include <SD.h>
-
-const int chipSelect = 4;
-
-Sd2Card card;
-
-#define DEBUG
-
-void setup() {
-  #ifdef DEBUG
-  Serial.begin(9600);
-  Serial.println("[System Init]");
-  
-  Serial.println("[SD Card Init]");
-  #endif
-  
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card is inserted?");
-    Serial.println("* Is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    return;
-  } else {
-   Serial.println("Wiring is correct and a card is present."); 
-  }
-  
-  Serial.print("\nCard type: ");
-  switch(card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
-
-  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-  if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-    return;
-  }
-
-
-  // print the type and size of the first FAT-type volume
-  uint32_t volumesize;
-  Serial.print("\nVolume type is FAT");
-  Serial.println(volume.fatType(), DEC);
-  Serial.println();
-  
-  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-  volumesize *= 512;                            // SD card blocks are always 512 bytes
-  Serial.print("Volume size (bytes): ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Kbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-
-  
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-  root.openRoot(volume);
-  
-  // list all files in the card with date and size
-  root.ls(LS_R | LS_DATE | LS_SIZE);
-  
-  
-}
-
-void loop(){
-
-}
-
-*/
